a41f573007ecbdeb3c9a77f6cfb307ac4c3d925f
Grumete86 <bruno.pradanos@gmail.com>
2024-10-14 18:22:21 +0200

    jsonify5

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ response: "succesfulResponse" }));
+    res.json({ response: "succesfulResponse" });
 });
 
 app.listen(5000, () => {
